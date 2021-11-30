7c44dc2d0a61c639814aab34bf50bee48b9667cc
giotmuabuon128 <giotmuabuon128@github.com>
2021-11-30 04:21:55 +0000

    Add book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_030323) do
+ActiveRecord::Schema.define(version: 2021_11_30_042044) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "number_of_pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
